efaef9d29b6fe2912541162df9a82c1f7130d875
Pedro Lucas <pdrolucas.dev@gmail.com>
2022-11-02 15:56:17 -0300

    feat: aula 1

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,50 @@
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
+
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  console.log('Query: ', e.query);
+  console.log('Prams: ', e.params);
+  console.log('Duration: ', e.duration + 'ms');
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get('/pools/count', () => {
-    return { count: 121331 };
+  await fastify.register(cors, {
+    origin: true,
   });
 
-  await fastify.listen({ port: 3333 });
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
