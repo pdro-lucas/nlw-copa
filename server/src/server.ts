97ce6b7a49877c459d266046221fac675092762d
Pedro Lucas <pdrolucas.dev@gmail.com>
2022-11-02 18:06:05 -0300

    feat: server new features

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
+import ShortUniqueId from 'short-unique-id';
+import { z } from 'zod';
 
 const prisma = new PrismaClient({
   log: [
@@ -38,12 +40,50 @@ async function bootstrap() {
     origin: true,
   });
 
+  // ROUTES
+  // Get pools count
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count();
 
     return { count };
   });
 
+  // Create new Pool
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
+  });
+
+  // Get users count
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  // Get guesses count
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
+  // init server
   await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
